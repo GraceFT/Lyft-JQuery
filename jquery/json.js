4c888d6ce2a154d7f1f33d22afd0ee921c4e8bb0
Grace <gracefctorres@gmail.com>
2017-02-24 06:18:18 -0500

    fixed select-countries, adding 1 country

--- jquery/json.js
+++ jquery/json.js
@@ -1,4 +1,5 @@
 var jsoncountry='[{"nombre": "Australia","code":"+61", "image":"images/flags/flat/16/Australia.png"},'+
+'{"nombre":"Belgica","code":"+32","image":"images/flags/flat/16/Belgium.png"},'+
 '{"nombre":"Canada","code":"+1","image":"images/flags/flat/16/Canada.png"},'+
 '{"nombre":"China","code":"+86","image":"images/flags/flat/16/China.png"},'+
 '{"nombre":"France","code": "+33", "image":"images/flags/flat/16/France.png"},'+
